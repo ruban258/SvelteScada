dc5dfaa842b9b641150a9c4592c18bfd2dedceb1
gajaruban.jeyakumar@gmail.com <gajaruban.jeyakumar@gmail.com>
2021-04-29 23:56:53 +0200

    Wireable store is now a json with key.

--- src/SvelteScada/svelte-app/App.svelte
+++ src/SvelteScada/svelte-app/App.svelte
@@ -3,6 +3,7 @@
 	import { setClient } from "svelte-apollo";
   import {allTags} from './Stores.js';
   import {client, READ_ALL_TAGS, READ_TAG} from './GraphQLClient';
+  import Navbar from './Navbar.svelte';
   
   setClient(client);
 
@@ -14,42 +15,22 @@
       console.log("loading")}
     else if (event.data){
     let localtags = event.data.tags;  
-    allTags.set(localtags);
+    localtags.forEach(element => {
+      let tagName = element.tagName;
+      let value = element.value;
+      $allTags[tagName] = {value}
+    });    
   }});
-
-  //tags.subscribe(event => console.log(JSON.stringify(event.data)));
+  
   tag.subscribe(event =>{
       if($tags.loading){    
       console.log("loading")}
     else if ($tags.data && $allTags){    
-      $allTags = $allTags.map(p => p.tagName === event.data.onTagUpdated.tagName ? { ...p, value: event.data.onTagUpdated.value }: p);
+      $allTags[event.data.onTagUpdated.tagName].value = event.data.onTagUpdated.value;      
     }});  
 </script>
 <main>
-  <nav class="bg-blue-900 shadow-lg">
-    <div class="container mx-auto">
-      <div class="sm:flex">
-        <a href class="text-white text-3xl font-bold p-3">APP LOGO</a>       
-        <!-- Menus -->
-        <div class="ml-55 mt-4">
-          <ul class="text-white sm:self-center text-xl">
-            <li class="sm:inline-block">
-              <a href class="p-3 hover:text-red-900">About</a>
-            </li>
-            <li class="sm:inline-block">
-              <a href class="p-3 hover:text-red-900">Services</a>
-            </li>
-            <li class="sm:inline-block">
-              <a href class="p-3 hover:text-red-900">Blog</a>
-            </li>
-            <li class="sm:inline-block">
-              <a href class="p-3 hover:text-red-900">Contact</a>
-            </li>
-          </ul>
-        </div>  
-      </div>
-    </div>
-  </nav>
+  <Navbar></Navbar>
 </main>
 
 
--- src/SvelteScada/svelte-app/Stores.js
+++ src/SvelteScada/svelte-app/Stores.js
@@ -1,3 +1,3 @@
 import { writable } from 'svelte/store';
 
-export var allTags = writable([]);
+export var allTags = writable({});
